9443452df94a8cd6b962dbabd9a8234b865b96c3
Genevieve Savard <gen.svrd@outlook.com>
2023-02-06 13:32:38 +0100

    Modify setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='NoisePy-ant',
     version='1.0.0',
-    packages=['noisepy'],
+    packages=['noisepy', 'dispvelmaps'],
     url='',
     license='',
     author='Genevieve Savard',
