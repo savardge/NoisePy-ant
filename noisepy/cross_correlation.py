372ff03be44d59b62fdb18cf8dc8b6d545c2d4c7
Genevieve Savard <gen.svrd@outlook.com>
2023-05-02 21:10:33 +0200

    Disable numba for moving_ave function

--- noisepy/cross_correlation.py
+++ noisepy/cross_correlation.py
@@ -1006,7 +1006,7 @@ def cc_parameters(cc_para, coor, tcorr, ncorr, comp):
 
 
 # Utility functions *******************************************
-@jit(nopython=True)
+#@jit(nopython=True)
 def moving_ave(A, N):  ## change the moving average calculation to take as input N the full window length to smooth
     '''
     Alternative function for moving average for an array.
