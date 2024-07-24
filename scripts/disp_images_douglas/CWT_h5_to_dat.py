af8a8d7055512e992ae3f7e8d01eed9ecbc7b6d5
Genevieve Savard <gen.svrd@outlook.com>
2024-07-24 10:39:08 -0400

    Add files from Douglas

---
+++ scripts/disp_images_douglas/CWT_h5_to_dat.py
@@ -0,0 +1,278 @@
+import pycwt
+import numpy as np
+import math
+from cmath import inf
+import scipy
+from scipy.fftpack import fft, ifft
+from scipy.interpolate import interp1d
+from scipy.signal import hilbert, windows, firwin, lfilter
+from scipy.signal.windows import tukey as tukey_window
+import matplotlib.pyplot as plt
+import obspy
+import os
+import glob
+import pyasdf
+import sys
+
+""" 
+Necessary modules and functions
+
+Make sure to download the packages pycwt, scipy, pyasdf, obspy and matplotlib
+"""
+
+
+def get_disp_image_taper(ccf, dist, dt, Tmin=0.2, Tmax=7, dT=0.01, vmin=0.5, vmax=3.5, dvel=0.005, vave=3., plot=True,
+                         figsize=(14, 6)):
+    '''
+    Get the group dispersion image with the Continuous Wavelet Transform
+    Args:
+        ccf: Cross-correlation function (symmetric)
+        dist: inter-station distance for the given ccf in km
+        dt: Sampling interval in second
+        Tmin: Minimum period
+        dT: Spacing of the period axis on the dispersion image in second
+        vmin: Minimum group velocity
+        vmax: Maximum group velocity
+        dvel: Spacing of the group velocity axis in km/s
+        vave: average velocity in km/s used to calculate Tmax = dist/ vave
+        plot: Whether to plot or not (bool)
+        figsize: Figure size if plotting
+
+    Returns:
+        rcwt_new: Group dispersion image (2D numpy array)
+        per, vel: corresponding period and group velocity vectors
+    '''
+    ccf = ccf.copy()
+
+    # Basic parameters for wavelet transform
+    dj = 1 / 120  # Spacing between discrete scales. Default is Twelve sub-octaves per octaves.
+    # Smaller values will result in better scale resolution, but slower calculation and plot.
+    s0 = -1  # Smallest scale of the wavelet. Default value [-1] is 2*dt.
+    J = -1  # Number of scales less one.
+    # Scales range from s0 up to s0 * 2**(J * dj), which gives a total of (J + 1) scales.
+    # Default [-1] is J = (log2(N * dt / so)) / dj.
+    wvn = 'morlet'  # type of wavelet to use
+
+    # Get period and velocity ranges
+    # Tmax = dist / vave  # Max period assumes a velocity of 3 km/s
+    fmin = 1 / Tmax
+    fmax = 1 / Tmin
+
+    # Douglas - added +dT +dvel to have consistent shape
+    per = np.arange(Tmin, Tmax + dT, dT)  # Periods
+    vel = np.arange(vmin, vmax + dvel, dvel)  # Group velocities
+
+    # Trim the CCF according to velocity window vmin-vmax
+    npts = ccf.shape[0]
+    tvec = np.arange(0, npts) * dt
+    pt1 = int(dist / vmax / dt)
+    pt2 = int(dist / vmin / dt)
+    if pt1 == 0:
+        pt1 = 10
+    if pt2 > (npts // 2):
+        pt2 = npts // 2
+    indx = np.arange(pt1, pt2)
+
+    # Taper the window
+    taper = np.zeros(shape=npts)
+    window = tukey_window(len(indx), alpha=0.05, sym=True)
+    taper[indx] = window
+    ccf *= taper
+    # Cut the part after the taper (to speed up calculation)
+    ccf = ccf[:pt2]
+    tvec = tvec[:pt2]
+
+    # wavelet transformation
+    cwt, sj, freq, coi, _, _ = pycwt.cwt(ccf, dt, dj, s0, J, wvn)
+
+    # Filter the image within the requested frequency band
+    if (fmax > np.max(freq)) | (fmax <= fmin):
+        raise ValueError('Abort: frequency out of limits!')
+    freq_ind = np.where((freq >= fmin) & (freq <= fmax))[0]
+    cwt = cwt[freq_ind]
+    freq = freq[freq_ind]
+
+    # Calculate the amplitude and phase of the cwt
+    period = 1 / freq
+    rcwt = np.abs(cwt) ** 2  # Amplitude
+    pcwt = np.angle(cwt)  # Phase
+
+    # Remove t=0 sample
+    tvec = tvec[1:]
+    rcwt = rcwt[:, 1:]
+    pcwt = pcwt[:, 1:]
+    coi = coi[1:]
+
+    # Interpolation of the image to the requested intervals in period and velocity
+    velocity = dist / tvec
+    fc = scipy.interpolate.interp2d(velocity, period, rcwt)
+    rcwt_new = fc(vel, per)
+    fp = scipy.interpolate.interp2d(velocity, period, pcwt)
+    pcwt_new = fp(vel, per)
+
+    # Interpolation of coi
+    ff = scipy.interpolate.interp1d(velocity, coi, fill_value='extrapolate', assume_sorted=False)
+    coi_new = ff(vel)
+
+    # Normalization amplitude at each frequency
+    rcwt_new /= np.max(rcwt_new, axis=1)[:, np.newaxis]
+
+    # Plot
+    if plot:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+        ax.imshow(np.transpose(rcwt_new),
+                  cmap='jet',
+                  extent=[per[0], per[-1], vel[0], vel[-1]],
+                  aspect='auto',
+                  origin='lower')
+        ax.scatter(coi_new, vel, c="k", s=5)
+        ax.set(xlabel='Period [s]', ylabel='Vg [km/s]', xlim=(Tmin, Tmax), ylim=(vmin, vmax))
+        ax.set_title('Inter-station distance: %5.2f km' % dist)
+        plt.tight_layout()
+        plt.show()
+        plt.close()
+
+    return rcwt_new, pcwt_new, per, vel, coi_new
+
+
+def generate_coi_mask(coi_new, vel_grid, per_grid):
+    mask = np.zeros_like(vel_grid)
+
+    for i in range(mask.shape[0]):
+        for j in range(mask.shape[1]):
+            if per_grid[i, j] < coi_new[j]:
+                mask[i, j] = 1
+            else:
+                mask[i, j] = 0
+
+    return mask
+
+
+# Not needed but keep in case
+# def fold_trace(tr):
+#    """fold_trace() takes a trace and computes the symmetric component by 
+#    summing the time-reversed acausal component with the causal coponent"""
+# Pick some constants we'll need from the trace:
+#    npts2 = tr.stats.npts
+#    npts = int((npts2-1)/2)
+
+# Fold the CCFs:
+#    tr_folded= tr.copy()
+#    causal =  tr.data[npts:-1]
+#    acausal = tr.data[npts:0:-1]
+#    tr_folded.data =  (causal+acausal)/2
+
+#    return tr_folded
+
+###########################################################################################
+""" 
+Import data from NoisePy stack files
+"""
+
+# Specify stack directory:
+stack_dir = "/srv/beegfs/scratch/shares/cdff/MIGRATE/Tuscany_July2023/noisepyH5/stack-1bit"
+export_cwt_path = "/home/users/s/stumpp4/scratch/RoccaStrada/E_FTAN_and_picking/B_automatic_picking/CWT_DisperPicker/data/TestData/group_image/"
+export_coi_path = "/home/users/s/stumpp4/scratch/RoccaStrada/E_FTAN_and_picking/CWT_coi_dat/"
+
+filelist = glob.glob(os.path.join(stack_dir, "*", "*.h5"))
+filelist.sort()
+
+""" 
+Select the stacking method. Options: 
+- Allstack_linear (linear stacking, taking the mean)
+- Allstack_pws (phase weighted stack) 
+- Allstack_nroot (N root stack Millet, F et al., 2019 JGR, with power=2)
+- Allstack_robust (Palvis and Vernon 2010)
+- Allstack_auto_covariance (Adaptive filter of Nakata et al., 2015 appendix B: with filter harshness g=1)
+"""
+stack_method = "Allstack_pws"
+
+"""
+Select the component: 
+Options: ['EE', 'EN', 'EZ', 'NE', 'NN', 'NZ', 'RR', 'RT', 'RZ', 'TR', 'TT', 'TZ', 'ZE', 'ZN', 'ZR', 'ZT', 'ZZ']
+"""
+component = "ZZ"
+
+# Setup up period and velocity limits and spacing
+vmin = 0.5
+vmax = 3.5
+dvel = 0.005
+Tmin = 0.2
+Tmax = 7
+dT = 0.01
+
+# Loop over files to check them out
+for fname in filelist:
+
+    print(f"Processing this file: {fname}")
+    try:
+        with pyasdf.ASDFDataSet(fname, mode="r") as ds:
+            # print(ds.auxiliary_data.list()) # This shows the list of stack methods available
+            # print(ds.auxiliary_data[stack_method].list()) # This shows the list of components available
+            ccf_full = ds.auxiliary_data[stack_method][component].data[:]
+            params = ds.auxiliary_data[stack_method][component].parameters
+
+        pair = os.path.split(fname)[1].split(".h5")[0]
+        # print(f"pair: {pair}, distance: {params['dist']}")
+
+        # Make an Obspy trace for full symmetric component
+        tr_full = obspy.Trace(ccf_full, header={'delta': params['dt'], 'station': pair, 'channel': component,
+                                                'distance': params['dist']})
+        npts2 = tr_full.stats.npts
+        npts = int((npts2 - 1) / 2)
+        causal = tr_full.data[npts:-1]
+        acausal = tr_full.data[npts:0:-1]
+
+        # Get the data
+        dt = tr_full.stats.delta
+        dist = params['dist']
+
+        # Maxamplitude fonction and CF to EGFs conversion through hilbert transform
+        PtNum = npts
+        samp_rate = tr_full.stats.sampling_rate  # Sampling frequency (SampleF)
+        Time = np.arange(0, (PtNum - 1) * dt, dt)
+
+        maxamp = max(max(causal), max(acausal))
+
+        if maxamp > 0:
+            causal_new = causal / maxamp
+            acausal_new = acausal / maxamp
+
+        Green_causal = np.imag(hilbert(causal_new))
+        Green_acausal = np.imag(hilbert(acausal_new))
+        stackEGF = (Green_causal + Green_acausal) / 2.0
+
+        ccf = stackEGF
+
+        amp, phase, per, vel, coi = get_disp_image_taper(ccf,  # numpy array of the symmetric CCF
+                                                         dist,  # inter-station distance in km
+                                                         dt,  # sampling interval in seconds
+                                                         Tmin=Tmin,
+                                                         Tmax=Tmax,  # min period
+                                                         dT=dT,  # period step
+                                                         vmin=vmin,  # min Vg
+                                                         vmax=vmax,  # max Vg
+                                                         dvel=dvel,  # steps in Vg
+                                                         plot=False)
+
+        vel_grid, per_grid = np.meshgrid(vel, per)
+        coi_mask = generate_coi_mask(coi, vel_grid, per_grid)
+
+        parts = pair.split('_')
+
+        # 1. cwt amplitude
+        cwt_export = np.transpose(amp)
+        cwt_name_export = parts[0].split('.')[1] + '.' + parts[1].split('.')[1] + '.dat'
+        np.savetxt(os.path.join(export_cwt_path, cwt_name_export), cwt_export)
+
+        # 2. Coi mask/line
+        coi_mask_export = np.flip(np.transpose(coi_mask))
+        coi_mask_name_export = 'mask_' + parts[0].split('.')[1] + '.' + parts[1].split('.')[1] + '.dat'
+        np.savetxt(os.path.join(export_coi_path, coi_mask_name_export), coi_mask_export)
+        # coi_line_export = coi
+        # coi_line_name_export = 'scatter_' + parts[0].split('.')[1] + '.' + parts[1].split('.')[1] + '.dat'
+        # np.savetxt(os.path.join(export_coi_path,coi_line_name_export),coi_line_export)
+
+
+    except:
+        print(f"Could not export CWT for file {fname}")
---
+++ scripts/disp_images_douglas/TimeShift_PhaseSpectrograms_h5_to_dat_1file.py
@@ -0,0 +1,253 @@
+import numpy as np
+import math
+from cmath import inf
+import scipy
+from scipy.signal import hilbert, windows, firwin, lfilter
+from scipy.fftpack import fft, ifft
+from scipy.interpolate import interp1d
+from scipy.signal.windows import tukey as tukey_window
+import matplotlib.pyplot as plt
+import obspy
+import os
+import glob
+import pyasdf
+import sys
+import yaml
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
+logger = logging.getLogger(__name__)
+
+
+def generate_phase(stackEGF, fs, vmin, vmax, dvel, Tmin, Tmax, dT, dist, npts, Time, dt):
+    # find relevant ccf window index based on interstation distance and velocity range
+    g_WinMaxPtNum = round(fs * dist // vmin)
+    g_WinMinPtNum = round(fs * dist // vmax)
+
+    if g_WinMaxPtNum >= npts:
+        g_WinMaxPtNum = npts - 1
+        vmin = np.ceil(10 * dist / Time[-1]) / 10
+        logger.warning(f"Min velocity reset to: {vmin}")
+
+    # number of time and velocity points
+    NumCtrT = round((Tmax - Tmin) / dT) + 1
+    # Tpoint  = np.linspace(Tmin, Tmax, NumCtrT)
+    NumCtrV = round((vmax - vmin) / dvel) + 1
+    Vpoint = np.linspace(vmax, vmin, NumCtrV)
+
+    # calculate the window function
+    Window, TaperLen = generate_signal_window(g_WinMinPtNum, g_WinMaxPtNum, npts, winalpha=0.05)
+    # Window the stacked EGF (causal + acausal)
+    WinWave = stackEGF * Window
+
+    # Extract Noise window after the windowed surface wave 
+    WinWaveClip, WaveClipPt = extract_noise_window(dt, TaperLen, g_WinMaxPtNum, npts, stackEGF, WinWave, NoiseTime=150)
+
+    PhaseVIm = time_shift(WinWaveClip, WaveClipPt, fs, Vpoint, Tmin, dT, dist, dt, NumCtrT, g_WinMinPtNum,
+                          g_WinMaxPtNum, FilterKaiserPara=6, MaxFilterLengthLog=13)
+
+    return PhaseVIm
+
+
+def generate_signal_window(g_WinMinPtNum, g_WinMaxPtNum, npts, winalpha):
+    # window length
+    win_len = int((g_WinMaxPtNum - g_WinMinPtNum) / (1 - winalpha)) + 1
+    # generate window function
+    Window = windows.tukey(win_len, winalpha)
+    TaperLen = round(win_len * winalpha / 2)
+
+    # crop or add the left side
+    pad_left_len = int(g_WinMinPtNum) - TaperLen
+    if pad_left_len > 0:
+        Window = np.pad(Window, (pad_left_len, 0), 'constant')
+    else:
+        Window = Window[-pad_left_len:]
+    # crop or add the right side
+    if Window.shape[0] < npts:
+        Window = np.pad(Window, (0, npts - Window.shape[0]), 'constant')
+    else:
+        Window = Window[:npts]
+
+    return Window, TaperLen
+
+
+def extract_noise_window(dt, TaperLen, g_WinMaxPtNum, npts, stackEGF, WinWave, NoiseTime=150):
+    # Extract Noise window after the windowed surface wave       
+    NoisePt = round(NoiseTime / dt)
+    NoiseStartIndex = int(g_WinMaxPtNum) + TaperLen
+
+    if (NoiseStartIndex + NoisePt) < npts:
+        NoiseWinWave = stackEGF[NoiseStartIndex:NoiseStartIndex + NoisePt]
+    else:
+        NoiseWinWave = stackEGF[NoiseStartIndex:]
+        NoisePt = len(NoiseWinWave)
+        logger.warning(f"Noise Window Length of {NoiseWinWave.shape[0]} is not long enough")
+
+    WaveClipPt = min((int(g_WinMaxPtNum) + TaperLen), npts)
+    WinWaveClip = WinWave[:WaveClipPt]
+
+    return WinWaveClip, WaveClipPt
+
+
+def time_shift(WinWaveClip, WaveClipPt, fs, Vpoint, Tmin, dT, dist, dt, NumCtrT, g_WinMinPtNum, g_WinMaxPtNum,
+               FilterKaiserPara=6, MaxFilterLengthLog=13):
+    # Apply time shift method in time domain
+
+    BandWidth = dT
+
+    exponential = min(math.ceil(np.log2(1024 * fs)), MaxFilterLengthLog)
+    filter_length = int(2 ** exponential)
+
+    HalfFilterNum = int(filter_length / 2)
+
+    WinWave2 = np.concatenate((np.copy(WinWaveClip), np.zeros(HalfFilterNum)))
+
+    PhaseIm = []
+    for numt in range(NumCtrT):
+        CtrT = Tmin + numt * dT
+        # CtrF  = (2 / fs) / CtrT
+        LowF = (2 / fs) / (CtrT + 0.5 * BandWidth)
+        HighF = (2 / fs) / (CtrT - 0.5 * BandWidth)
+
+        filter_data = firwin(filter_length + 1, [LowF, HighF], pass_zero=False, window=('kaiser', FilterKaiserPara))
+
+        # We do not have information of reference group velocity window for using 
+        # frequency-time filtering analysis on variable MFT(?) to window the original waveform
+
+        # Therefore, rather use uniform group velocity window and apply phase shift technique in time domain (?)
+        # i.e. two-pass filtering (time and time-reverse) in order to remove phase shift
+
+        # filtering
+        FilteredWave = lfilter(filter_data, 1, WinWave2)
+        # inverse order
+        FilteredWave = FilteredWave[::-1]
+        # filtering
+        FilteredWave = lfilter(filter_data, 1, FilteredWave)
+        # inverse order
+        FilteredWave = FilteredWave[::-1]
+        # clip
+        FilteredWave = (FilteredWave[:WaveClipPt])
+        # normalization
+        FilteredWave = FilteredWave / np.max(np.abs(FilteredWave))
+        PhaseIm.append(FilteredWave)
+
+    timeptnum = np.array(range(int(g_WinMinPtNum), int(g_WinMaxPtNum)))
+    phase_time = timeptnum * dt
+    PhaseVIm = []
+
+    for i in range(NumCtrT):
+        CenterT = Tmin + i * dT
+        TravPtV = dist / (phase_time - CenterT / 8)
+
+        # time - CenterT/8 may be zero
+        TravPtV[TravPtV == inf] = 100
+
+        PhaseVIm.append(interp1d(TravPtV[::-1], (PhaseIm[i][int(g_WinMinPtNum):int(g_WinMaxPtNum)])[::-1], kind='cubic',
+                                 bounds_error=False, fill_value=0)(Vpoint))
+
+    PhaseVIm = np.transpose(np.array(PhaseVIm))
+    # reverse
+    PhaseVIm = PhaseVIm[::-1]
+
+    return PhaseVIm
+
+
+# Not needed but keep in case
+# def fold_trace(tr):
+#    """fold_trace() takes a trace and computes the symmetric component by 
+#    summing the time-reversed acausal component with the causal coponent"""
+# Pick some constants we'll need from the trace:
+#    npts2 = tr.stats.npts
+#    npts = int((npts2-1)/2)
+
+# Fold the CCFs:
+#    tr_folded= tr.copy()
+#    causal =  tr.data[npts:-1]
+#    acausal = tr.data[npts:0:-1]
+#    tr_folded.data =  (causal+acausal)/2
+
+#    return tr_folded
+
+################################################################
+
+# Input file name from command line argument:
+fname = sys.argv[1]
+print(f"Processing this file: {fname}")
+
+# Specify output directory:
+export_path = "/home/itopie/Bureau/N001_phase_image/"
+
+""" 
+Select the stacking method. Options: 
+- Allstack_linear (linear stacking, taking the mean)
+- Allstack_pws (phase weighted stack) 
+- Allstack_nroot (N root stack Millet, F et al., 2019 JGR, with power=2)
+- Allstack_robust (Palvis and Vernon 2010)
+- Allstack_auto_covariance (Adaptive filter of Nakata et al., 2015 appendix B: with filter harshness g=1)
+"""
+stack_method = "Allstack_pws"
+
+"""
+Select the component: 
+Options: ['EE', 'EN', 'EZ', 'NE', 'NN', 'NZ', 'RR', 'RT', 'RZ', 'TR', 'TT', 'TZ', 'ZE', 'ZN', 'ZR', 'ZT', 'ZZ']
+"""
+component = "ZZ"
+
+# Setup up period and velocity limits and spacing
+vmin = 0.5
+vmax = 3.5
+dvel = 0.005
+Tmin = 0.2  # Tmin should not be at the limit given by 1/(2*fs)
+Tmax = 7
+dT = 0.01
+
+try:
+    with pyasdf.ASDFDataSet(fname, mode="r") as ds:
+        # print(ds.auxiliary_data.list()) # This shows the list of stack methods available
+        # print(ds.auxiliary_data[stack_method].list()) # This shows the list of components available
+        ccf_full = ds.auxiliary_data[stack_method][component].data[:]
+        params = ds.auxiliary_data[stack_method][component].parameters
+
+    pair = os.path.split(fname)[1].split(".h5")[0]
+    # print(f"pair: {pair}, distance: {params['dist']}")
+
+    # Make an Obspy trace for full symmetric component
+    tr_full = obspy.Trace(ccf_full, header={'delta': params['dt'], 'station': pair, 'channel': component,
+                                            'distance': params['dist']})
+    npts2 = tr_full.stats.npts
+    npts = int((npts2 - 1) / 2)
+    causal = tr_full.data[npts:-1]
+    acausal = tr_full.data[npts:0:-1]
+
+    # Get the data
+    dt = tr_full.stats.delta
+    dist = params['dist']
+
+    # Maxamplitude fonction and CF to EGFs conversion through hilbert transform
+    PtNum = npts
+    fs = tr_full.stats.sampling_rate  # Sampling frequency (SampleF)
+    Time = np.arange(0, (PtNum - 1) * dt, dt)
+
+    maxamp = max(max(causal), max(acausal))
+
+    if maxamp > 0:
+        causal_new = causal / maxamp
+        acausal_new = acausal / maxamp
+
+        Green_causal = np.imag(hilbert(causal_new))
+        Green_acausal = np.imag(hilbert(acausal_new))
+        stackEGF = (Green_causal + Green_acausal) / 2.0
+
+        PhaseVIm = generate_phase(stackEGF, fs, vmin, vmax, dvel, Tmin, Tmax, dT, dist, npts, Time, dt)
+
+        parts = pair.split('_')
+
+        phase_name_export = parts[0].split('.')[1] + '.' + parts[1].split('.')[1] + '.dat'
+        np.savetxt(os.path.join(export_path, phase_name_export), PhaseVIm)
+    else:
+        print("maxamp is 0 for the CCF.... skipping")
+
+except:
+    print(f"Could not export Phase spectrogram for file {fname}")
